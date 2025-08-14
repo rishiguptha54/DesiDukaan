444f240f695d7da8df3facf5d9df99a9ba2f7366
Rishi Guptha <rishi2002guptha@gmail.com>
2025-08-14 21:22:36 +0530

    link

--- client/src/store/admin/order-slice/index.js
+++ client/src/store/admin/order-slice/index.js
@@ -1,5 +1,95 @@
+// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// import axios from "axios";
+
+// const initialState = {
+//   orderList: [],
+//   orderDetails: null,
+// };
+
+
+// export const getAllOrdersForAdmin = createAsyncThunk(
+//   "/order/getAllOrdersForAdmin",
+//   async () => {
+//     const response = await axios.get(
+//       `http://localhost:5000/api/admin/orders/get`
+//     );
+
+//     return response.data;
+//   }
+// );
+
+// export const getOrderDetailsForAdmin = createAsyncThunk(
+//   "/order/getOrderDetailsForAdmin",
+//   async (id) => {
+//     const response = await axios.get(
+//       `http://localhost:5000/api/admin/orders/details/${id}`
+//     );
+
+//     return response.data;
+//   }
+// );
+
+// export const updateOrderStatus = createAsyncThunk(
+//   "/order/updateOrderStatus",
+//   async ({ id, orderStatus }) => {
+//     const response = await axios.put(
+//       `http://localhost:5000/api/admin/orders/update/${id}`,
+//       {
+//         orderStatus,
+//       }
+//     );
+
+//     return response.data;
+//   }
+// );
+
+// const adminOrderSlice = createSlice({
+//   name: "adminOrderSlice",
+//   initialState,
+//   reducers: {
+//     resetOrderDetails: (state) => {
+//       console.log("resetOrderDetails");
+
+//       state.orderDetails = null;
+//     },
+//   },
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(getAllOrdersForAdmin.pending, (state) => {
+//         state.isLoading = true;
+//       })
+//       .addCase(getAllOrdersForAdmin.fulfilled, (state, action) => {
+//         state.isLoading = false;
+//         state.orderList = action.payload.data;
+//       })
+//       .addCase(getAllOrdersForAdmin.rejected, (state) => {
+//         state.isLoading = false;
+//         state.orderList = [];
+//       })
+//       .addCase(getOrderDetailsForAdmin.pending, (state) => {
+//         state.isLoading = true;
+//       })
+//       .addCase(getOrderDetailsForAdmin.fulfilled, (state, action) => {
+//         state.isLoading = false;
+//         state.orderDetails = action.payload.data;
+//       })
+//       .addCase(getOrderDetailsForAdmin.rejected, (state) => {
+//         state.isLoading = false;
+//         state.orderDetails = null;
+//       });
+//   },
+// });
+
+// export const { resetOrderDetails } = adminOrderSlice.actions;
+
+// export default adminOrderSlice.reducer;
+
+
+
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
+
+const API_BASE_URL = "https://desidukaan.onrender.com/api";
 
 const initialState = {
   orderList: [],
@@ -9,10 +99,7 @@ const initialState = {
 export const getAllOrdersForAdmin = createAsyncThunk(
   "/order/getAllOrdersForAdmin",
   async () => {
-    const response = await axios.get(
-      `http://localhost:5000/api/admin/orders/get`
-    );
-
+    const response = await axios.get(`${API_BASE_URL}/admin/orders/get`);
     return response.data;
   }
 );
@@ -20,10 +107,7 @@ export const getAllOrdersForAdmin = createAsyncThunk(
 export const getOrderDetailsForAdmin = createAsyncThunk(
   "/order/getOrderDetailsForAdmin",
   async (id) => {
-    const response = await axios.get(
-      `http://localhost:5000/api/admin/orders/details/${id}`
-    );
-
+    const response = await axios.get(`${API_BASE_URL}/admin/orders/details/${id}`);
     return response.data;
   }
 );
@@ -32,12 +116,9 @@ export const updateOrderStatus = createAsyncThunk(
   "/order/updateOrderStatus",
   async ({ id, orderStatus }) => {
     const response = await axios.put(
-      `http://localhost:5000/api/admin/orders/update/${id}`,
-      {
-        orderStatus,
-      }
+      `${API_BASE_URL}/admin/orders/update/${id}`,
+      { orderStatus }
     );
-
     return response.data;
   }
 );
@@ -48,7 +129,6 @@ const adminOrderSlice = createSlice({
   reducers: {
     resetOrderDetails: (state) => {
       console.log("resetOrderDetails");
-
       state.orderDetails = null;
     },
   },
@@ -82,7 +162,6 @@ const adminOrderSlice = createSlice({
 export const { resetOrderDetails } = adminOrderSlice.actions;
 
 export default adminOrderSlice.reducer;
-
 
 
 
